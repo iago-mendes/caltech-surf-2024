2b76f7a7589f9f091800a51b94cf3919a8b78f23
Iago Mendes <ibrazmen@oberlin.edu>
2024-11-15 16:27:04 -0500

    Update root-finding methods plot

--- plots/paper/root_finding_methods.py
+++ plots/paper/root_finding_methods.py
@@ -38,70 +38,38 @@ def get_data(dir):
     'residuals': residuals,
   }
 
-fig, axes = plt.subplots(1, 3, squeeze=True)
-
-col = 0
-data_min = np.Infinity
-data_max = -np.Infinity
+fig, ax = plt.subplots(1, 1, squeeze=True)
 
 def plot(title, args):
-  global col, data_min, data_max
-  ax = axes[col]
-  col += 1
-
   ax.set_title(title)
 
   for label, data in args:
     ax.plot(data['iterations'], data['residuals'], label=label)
 
-    data_min = min(data_min, np.min(data['residuals']))
-    data_max = max(data_max, np.max(data['residuals']))
-
-plot("Delayed/damped Broyden's method", [
-  [r'Delay first 2 iterations', get_data('./data/root_finding/delay_first_2_its')],
-  [r'Damp with $(1-e^{-k})$', get_data('./data/root_finding/damp_initial_iterations')],
-  [r'Diagonal', get_data('./data/root_finding/diagonal')],
-  [r'{scipy(broyden1)}: alpha = 1', get_data('./data/root_finding/scipy_noline')],
-  [r'{scipy(broyden1)}: alpha = 0.25', get_data('./data/root_finding/scipy_noline_damped')],
-])
-plot(r"\texttt{scipy(broyden1)} with line search", [
-  ['line_search = armijo, alpha = 1', get_data('./data/root_finding/scipy')],
-  ['line_search = armijo, alpha = 0.25', get_data('./data/root_finding/scipy_damped')],
-  ['line_search = wolfe, alpha = 1', get_data('./data/root_finding/scipy_wolfe_alpha1')],
-  ['line_search = wolfe, alpha = 0.25', get_data('./data/root_finding/scipy_wolfe')],
-])
-plot(r"\texttt{scipy(diagbroyden)}", [
-  ['line_search = None, alpha = 0.25', get_data('./data/root_finding/scipy_diag_noline_damped')],
-  ['line_search = armijo, alpha = 1', get_data('./data/root_finding/scipy_diag')],
-  ['line_search = armijo, alpha = 0.25', get_data('./data/root_finding/scipy_diag_damped')],
-  ['line_search = wolfe, alpha = 0.25', get_data('./data/root_finding/scipy_diag_damped_wolfe')],
+plot("Modified root-finding methods", [
+  [r'Delay first 2 iterations of $P_{ADM}$ and $C_{CoM}$', get_data('./data/root_finding/2024-11-11-delay')],
+  [r'Diagonal Jacobian', get_data('./data/root_finding/2024-11-11-Jacobian-diagonal')],
+  [r'Damp $\Delta u$ with $1-e^{-k}$', get_data('./data/root_finding//2024-11-11-FreeData-damping-expk')],
+  [r'Damp $\Delta J$ with $1-e^{-|\Delta F|^2/|\Delta u|^2}$', get_data('./data/root_finding/2024-11-11-Jacobian-damping-expDFDu')],
+  [r'Damp $\Delta J$ with $1-e^{-k}$', get_data('./data/root_finding/2024-11-15-Jacobian-damping-expk')],
+  [r'Damp $\Delta J$ with $1-e^{-k/2}$', get_data('./data/root_finding/2024-11-15-Jacobian-damping-expko2')],
+  [r'Damp $\Delta J$ with $1-e^{-k/4}$', get_data('./data/root_finding/2024-11-15-Jacobian-damping-expko4')],
 ])
 
-for ax in axes:
-  ax.set_xlabel(r'$k$')
-  ax.legend()
+ax.set_xlabel(r'$k$')
+ax.legend()
 
-  ax.set_xscale('linear')
-  ax.set_yscale('log')
+ax.set_xscale('linear')
+ax.set_yscale('log')
 
-  ax.set_ylim(data_min/1.5, data_max*1.5)
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.grid('on', linestyle='--', alpha=0.5)
 
-  ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-  ax.grid('on', linestyle='--', alpha=0.5)
-
-axes[0].set_ylabel(r'Control residual')
-
-axes[1].set_xlim(-0.5, 20.5)
-axes[2].set_xlim(-0.5, 20.5)
+ax.set_ylabel(r'Control residual')
 
 
-fig.set_size_inches(18,8)
+fig.set_size_inches(12,8)
 plt.tight_layout()
-plt.subplots_adjust(wspace=0.02)
-
-# Remove y tick labels
-for ax in axes[1:]:
-  ax.set_yticklabels([])
 
 fig.savefig(f'root_finding_methods.png', format='png', bbox_inches='tight')
 
